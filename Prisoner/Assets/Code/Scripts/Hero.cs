7a6e106b52a230829d7f71a960536ebb24f33f23
KSONIX2 <52291949+KSONIX2@users.noreply.github.com>
2022-10-03 20:19:42 +0300

    Revert "First commit"
    This reverts commit d57507b2fb1c24dca5e4041368126db8be535134.

--- Prisoner/Assets/Code/Scripts/Hero.cs
+++ Prisoner/Assets/Code/Scripts/Hero.cs
@@ -6,42 +6,26 @@ namespace Prisoner
     {
         [SerializeField] private float _speed;
         private Vector2 _direction;
-        private Rigidbody2D _rigidbody;
-
-        private void Awake()
-        {
-            _rigidbody = GetComponent<Rigidbody2D>();
-        }
-
         public void SetDirection(Vector2 direction)
         {
             _direction = direction;
         }
 
-        private void FixedUpdate()
+        private void Update()
         {
-            var xVelocity = _direction.x * _speed;
-            var yVelocity = CalculateYPosition();
+            var deltaX = _direction.x * _speed * Time.deltaTime;
+            var newXPosition = transform.position.x + deltaX;
+            var deltaY = _direction.y * _speed * Time.deltaTime;
+            var newYPosition = transform.position.y + deltaY;
 
-            _rigidbody.velocity = new Vector2(xVelocity, yVelocity);
-            //var deltaX = _direction.x * _speed * Time.deltaTime;
-            //var newXPosition = transform.position.x + deltaX;
-            //var deltaY = _direction.y * _speed * Time.deltaTime;
-            //var newYPosition = transform.position.y + deltaY;
-
-            //if (_direction.x != 0)
-            //{
-            //    transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
-            //}
-            //else if(_direction.y != 0)
-            //{
-            //    transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
-            //}
-        }
-
-        private float CalculateYPosition()
-        {
-            return default;
+            if (_direction.x != 0)
+            {
+                transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+            }
+            else if(_direction.y != 0)
+            {
+                transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+            }
         }
     }
 }
